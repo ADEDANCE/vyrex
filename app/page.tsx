e7c9b48fad18a623c9652bef1bda6a2b19bdb392
Adedance <uthmanadedance@gmail.com>
2026-04-23 23:53:42 +0100

    Instal shadcn and import couresel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import About from "./sections/About";
+import Hero from "./sections/Hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center  bg-zinc-50 font-sans dark:bg-black">
       <Navbar />
+      <Hero />
+      <About />
     </div>
   );
 }
