6bf55fc3596cb7bc6169b42d15253f733008b74d
Adedance <uthmanadedance@gmail.com>
2026-04-23 23:51:45 +0100

    Feat:Create reusable card

---
+++ app/components/Aboutcard.tsx
@@ -0,0 +1,32 @@
+type CardProps = {
+  icon?: React.ReactNode;
+  image?: React.ReactNode;
+  title?: string;
+  subtitle: string;
+  studentname?: string;
+};
+export default function Aboutcard({
+  icon,
+  title,
+  subtitle,
+  image,
+  studentname,
+}: CardProps) {
+  return (
+    <div className=" w-full shadow rounded-2xl border border-gray-200 bg-white py-4 px-4 text-start">
+      {icon && (
+        <div className=" bg-linear-to-bl from-blue-500 to-blue-300 flex items-center justify-center rounded-xl py-2 w-10 h-10 px-2">
+          {icon}
+        </div>
+      )}
+      <h2 className=" text-black text-xl font-bold ">{title}</h2>
+      <p>{subtitle}</p>
+      {image && (
+        <div className=" flex gap-4 items-center mt-3">
+          {image}
+          <p className=" font-bold">{studentname}</p>
+        </div>
+      )}
+    </div>
+  );
+}
