505e776bd0e155f4d86164f48c44940acb3e77dd
Adedance <uthmanadedance@gmail.com>
2026-04-29 18:10:49 +0100

    Feat: Create Paystack popup button

---
+++ app/components/PayButton.tsx
@@ -0,0 +1,76 @@
+"use client";
+import Button from "./Button";
+
+export default function PayButton() {
+  const verifyPayment = async (reference: string) => {
+    try {
+      //  call verify api
+      const res = await fetch("/api/verify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ reference }),
+      });
+
+      // Read API response
+      const data = await res.json();
+
+      // handle result
+      if (data.success) {
+        // redirect user
+        window.location.href = "/course/beginner";
+      } else {
+        alert("Payment verification failed");
+      }
+    } catch (err) {
+      alert("Network error");
+    }
+  };
+
+  // payment function
+  const handlePayment = () => {
+    // safety check
+    if (!(window as any).PaystackPop) {
+      alert("Paystack not loaded yet. Try again.");
+      return;
+    }
+    // Access Paystack global
+    const handler = (window as any).PaystackPop.setup({
+      // connect to Paystack account.
+      key: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY,
+
+      // Add REQUIRED fields
+      email: "test@email.com",
+      amount: 3000 * 100, // Paystack uses kobo, not naira.
+      ref: "" + Math.floor(Math.random() * 1000000000),
+
+      // Handle success
+      callback: function (response: any) {
+        console.log("Success:", response);
+
+        //  trigger next step
+        verifyPayment(response.reference);
+      },
+
+      // Handle cancel
+      onClose: function () {
+        console.log("Payment closed");
+      },
+    });
+    // open popup
+    handler.openIframe();
+  };
+
+  return (
+    <button
+      // onClick={() => {
+      //   console.log("clicked");
+      //   handlePayment();
+      // }}
+      onClick={handlePayment}
+    >
+      Pay Now
+    </button>
+  );
+}
