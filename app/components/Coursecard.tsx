50742ee241b77dee59859abb67134c8a2d0d5047
Adedance <uthmanadedance@gmail.com>
2026-04-29 18:09:36 +0100

    feat: Create reusable course card

---
+++ app/components/Coursecard.tsx
@@ -0,0 +1,33 @@
+type CourseCardProps = {
+  image: string;
+  title: string;
+  subtitle: string;
+  buttonText?: string;
+  className: string;
+  onClick?: () => void;
+};
+
+export default function CourseCard({
+  image,
+  title,
+  subtitle,
+  className,
+  buttonText = "Buy Now",
+  onClick,
+}: CourseCardProps) {
+  return (
+    <div className="w-full">
+      <img src={image} alt={title} className="rounded-t-2xl" />
+
+      <div className="bg-white rounded-b-2xl px-3 pb-3 space-y-2">
+        <h3 className="font-bold">{title}</h3>
+
+        <p className="text-xs text-gray-600">{subtitle}</p>
+
+        <button className={` px-3 py-1 rounded-lg ${className}`}>
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+}
