d8f797427d9b385ad64b3ae89322a3cefbe7291f
Adedance <uthmanadedance@gmail.com>
2026-05-04 17:52:10 +0100

    Add disable rops

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,6 +4,7 @@ type ButtonProps = {
   onClick?: () => void;
   className?: string;
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 };
 
 export default function Button({
@@ -12,11 +13,13 @@ export default function Button({
   onClick,
   className,
   type = "button",
+  disabled = false,
 }: ButtonProps) {
   return (
     <button
       type={type}
       onClick={onClick}
+      disabled={disabled}
       className={`px-4 py-2   transition ${className}`}
     >
       {children} {icon}
