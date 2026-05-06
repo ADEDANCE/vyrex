6bd956f8d9a0c65db13da2fa6cf0fdd1df8da749
Adedance <uthmanadedance@gmail.com>
2026-05-06 15:34:13 +0100

    Feat: import middleware

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,8 +1,59 @@
+"use client";
+import { useState } from "react";
 import Button from "../components/Button";
 import Textfield from "../components/Textfield";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const isFormValid = email.trim() && password.trim();
+
+  const isButtonDisabled = loading || !isFormValid;
+
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+
+      if (!email.trim() || !password.trim()) {
+        setMessage("All fields are required");
+        return;
+      }
+
+      // send requeest
+      const res = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include", // ensures cookies are accepted
+        body: JSON.stringify({ email, password }),
+      });
+
+      const result = await res.json();
+
+      if (result.success) {
+        setMessage(result.message);
+
+        setTimeout(() => {
+          router.push("/course");
+        }, 100);
+      } else {
+        setMessage(
+          // "Something went wrong",
+          result.error,
+        );
+      }
+    } catch (error) {
+      setMessage("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <section className=" bg-linear-to-b from-blue-200 to-blue-50  py-10 px-10 md:px-96 flex flex-col ">
       <div className="   flex flex-col items-center">
@@ -11,10 +62,24 @@ export default function Signup() {
           Log in to continue your creative journey.
         </p>
 
+        {message && (
+          <p
+            className={`mt-4 text-center text-sm ${
+              message.toLowerCase().includes("success")
+                ? "text-green-600"
+                : "text-red-500"
+            }`}
+          >
+            {message}
+          </p>
+        )}
+
         <Textfield
           className=" w-full mt-3"
           type="email"
           label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Example@email.com"
         />
 
@@ -22,6 +87,8 @@ export default function Signup() {
           className=" w-full mt-3"
           type="text"
           label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="At least 6 characters"
         />
       </div>
@@ -32,8 +99,14 @@ export default function Signup() {
       <div className="   flex flex-col items-center">
         {/* button */}
         <Button
-          children="Sign In"
-          className=" bg-linear-to-r from-blue-500 to-blue-300 rounded-xl mt-6 w-full text-white"
+          onClick={handleLogin}
+          disabled={loading}
+          children={loading ? "Signing in..." : "Sign In"}
+          className={
+            isButtonDisabled
+              ? "opacity-70 cursor-not-allowed rounded-xl mt-6 w-full text-white bg-blue-200"
+              : "bg-linear-to-r from-blue-500 to-blue-300 rounded-xl mt-6 w-full text-white"
+          }
         />
 
         <div className=" flex text-lg">
