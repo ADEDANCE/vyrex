6953d662db82b37824a48cd0e782b8fafe7106c2
Adedance <uthmanadedance@gmail.com>
2026-05-06 15:35:40 +0100

    Store token into cookies

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -2,64 +2,86 @@ import User from "../../../models/User";
 import bcrypt from "bcrypt";
 import { db } from "../../../lib/db";
 import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export async function POST(request) {
-  const body = await request.json();
-  const { email, password } = body;
+  try {
+    const body = await request.json();
+    const { email, password } = body;
 
-  // Connect to DB
-  await db();
+    if (!email || !password) {
+      return Response.json(
+        { success: false, error: "All fields are required " },
+        { status: 401 },
+      );
+    }
 
-  if (!email || !password) {
+    // Connect to DB
+    await db();
+
+    //   find user
+    const usermail = email.toLowerCase();
+
+    const user = await User.findOne({ email: usermail });
+
+    if (!user) {
+      return Response.json(
+        { success: false, error: "Invalid email or password" },
+        { status: 401 },
+      );
+    }
+
+    //   Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // Check password result
+    if (!isMatch) {
+      return Response.json(
+        { success: false, error: "Invalid email or password" },
+        { status: 400 },
+      );
+    }
+
+    // Generate JWT
+
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined");
+    }
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    // store cookies instance
+    const cookieStore = await cookies();
+    // use cookies
+    cookieStore.set("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      // secure: process.env.NODE_ENV === "production",
+      // sameSite: "strict",
+      maxAge: 60 * 60 * 24, // 1 day
+      path: "/",
+    });
+
+    //   delete user password
+    const userResponse = user.toObject();
+    delete userResponse.password;
+
     return Response.json(
-      { success: false, error: "All fields are required " },
-      { status: 400 },
+      {
+        success: true,
+        message: "Login successful",
+        // data: userResponse,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    return Response.json(
+      { success: false, error: error.message },
+      { status: 500 },
     );
   }
-
-  //   find user
-  const usermail = email.toLowerCase();
-
-  const user = await User.findOne({ email: usermail });
-
-  if (!user) {
-    return Response.json(
-      { success: false, error: "Invalid email or password" },
-      { status: 401 },
-    );
-  }
-
-  //   Compare password
-  const isMatch = await bcrypt.compare(body.password, user.password);
-
-  // Check password result
-  if (!isMatch) {
-    return Response.json(
-      { success: false, error: "Invalid email or password" },
-      { status: 400 },
-    );
-  }
-
-  // Generate JWT
-  const payload = { id: user._id, email: user.email, role: user.role };
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined");
-  }
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-
-  //   delete user password
-  const userResponse = user.toObject();
-  delete userResponse.password;
-
-  return Response.json(
-    {
-      success: true,
-      message: "Login successful",
-      token: token,
-      data: userResponse,
-    },
-    { status: 200 },
-  );
 }
