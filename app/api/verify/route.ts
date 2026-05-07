487324c419724a98c41c3a6c49422a9f7911929e
Adedance <uthmanadedance@gmail.com>
2026-05-07 02:20:49 +0100

    Refractor: make verification dynamic depending on the level paid for

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -9,12 +9,21 @@ export async function POST(req: Request) {
     // get the request body
     const body = await req.json();
     const reference = body.reference;
+    const level = body.level;
 
     // Validate reference
     if (!reference) {
       return NextResponse.json({
         success: false,
         message: "No reference provided",
+      });
+    }
+
+    // Validate level
+    if (!["beginner", "intermediate", "expert"].includes(level)) {
+      return NextResponse.json({
+        success: false,
+        message: "Invalid level",
       });
     }
 
@@ -35,13 +44,47 @@ export async function POST(req: Request) {
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-      userId = (decoded as any).id;
+      userId = (decoded as any).userId;
     } catch {
       return NextResponse.json({
         success: false,
         message: "Invalid token",
       });
     }
+
+    // Connect DB and fetch user
+
+    await db();
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Prevent skipping levels
+    if (level === "intermediate" && !user.progress.beginner.completed) {
+      return NextResponse.json({
+        success: false,
+        message: "Complete beginner first",
+      });
+    }
+
+    if (level === "expert" && !user.progress.intermediate.completed) {
+      return NextResponse.json({
+        success: false,
+        message: "Complete intermediate first",
+      });
+    }
+
+    // expected amount
+    let expectedAmount = 0;
+
+    if (level === "beginner") expectedAmount = 3000 * 100;
+    if (level === "intermediate") expectedAmount = 3000 * 100;
+    if (level === "expert") expectedAmount = 4000 * 100;
 
     // Call Paystack verification API
     const response = await fetch(
@@ -67,16 +110,36 @@ export async function POST(req: Request) {
 
     // Check if payment is successful
     if (data?.data?.status === "success") {
-      await db();
+      if (data.data.amount !== expectedAmount) {
+        return NextResponse.json({
+          success: false,
+          message: "Incorrect payment amount",
+        });
+      }
+      // beginner
+      if (level === "beginner") {
+        user.progress.beginner.paid = true;
+        user.currentLevel = "beginner";
+      }
 
-      // update the user
-      await User.findByIdAndUpdate(userId, {
-        beginnerPaid: true,
-        currentLevel: "beginner",
-      });
+      // Intermediate
+      if (level === "intermediate") {
+        user.progress.intermediate.paid = true;
+        user.currentLevel = "intermediate";
+      }
+
+      // expert
+      if (level === "expert") {
+        user.progress.expert.paid = true;
+        user.currentLevel = "expert";
+      }
+
+      // save user
+      await user.save();
+
       return NextResponse.json({
         success: true,
-        level: "beginner",
+        level,
       });
     }
 
