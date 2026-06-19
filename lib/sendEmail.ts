6f5fe1d75c701f1a36676b0189a0687882479c65
Adedance <uthmanadedance@gmail.com>
2026-06-19 04:10:57 +0100

    initialize Resend

---
+++ lib/sendEmail.ts
@@ -0,0 +1,105 @@
+import { Resend } from "resend";
+
+export async function sendWelcomeEmail(email: string, name: string) {
+  try {
+    // Resend instance
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    console.log("📧 Sending email to:", email);
+    const result = await resend.emails.send({
+      from: "VYREX <onboarding@resend.dev>",
+      to: email,
+      subject: "Welcome to VYREX ",
+      html: `
+        <h1>Welcome ${name}!</h1>
+
+        <p>
+          Thank you for joining VYREX.
+        </p>
+
+        <p>
+          Your video editing journey starts now.
+        </p>
+
+        <p>
+          Log in and begin your first lesson.
+        </p>
+      `,
+    });
+
+    console.log("EMAIL SENT:", result);
+  } catch (error) {
+    console.error("EMAIL ERROR:", error);
+  }
+}
+
+export async function sendCourseAccessEmail(
+  email: string,
+  name: string,
+  level: string,
+) {
+  try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    await resend.emails.send({
+      from: "VYREX <onboarding@resend.dev>",
+      to: email,
+      subject: `Your ${level} Level Access is Ready 🚀`,
+      html: `
+      <h1>Hello ${name},</h1>
+
+<p>Payment confirmed successfully! 🎉</p>
+
+<p>
+You now have access to the
+<strong>${level}</strong> Level.
+</p>
+
+<ul>
+<li>Lessons</li>
+<li>Resources</li>
+<li>Practical Exercises</li>
+</ul>
+
+<p>
+Login to continue learning.
+</p>`,
+    });
+  } catch (error) {
+    console.error("EMAIL ERROR:", error);
+  }
+}
+
+export async function sendCertificateEmail(
+  email: string,
+  name: string,
+  level: string,
+  courseLink: string,
+) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  await resend.emails.send({
+    from: "VYREX <onboarding@resend.dev>",
+    to: email,
+    subject: "Congratulations! You've Earned Your Certificate 🎉",
+    html: `
+      <div>
+        <h1>Congratulations ${name} 🎉</h1>
+
+        <p>
+          You have successfully completed the <strong>${level}</strong> level.
+        </p>
+
+        <p>
+          Your certificate is now ready.
+        </p>
+
+        <a href="${courseLink}" style="padding:10px 15px;background:#2563eb;color:white;text-decoration:none;border-radius:6px;">
+          View Certificate
+        </a>
+
+        <p>Keep learning and keep building 🚀</p>
+      </div>
+    `,
+  });
+}
